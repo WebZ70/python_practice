4d41cd68b3a8e8cd322330deb65c820efd1bd776
Tim <webz70@mail.ru>
2022-01-22 15:31:49 +0600

    Добален функция случайного удара

--- game/battle.py
+++ game/battle.py
@@ -13,9 +13,17 @@ def battle():
 
     print('\n\t\t В бой!\n')
     print('++++++++++ Журнал боя ++++++++++')
-    
 
-            
+    while len(list_of_war) > 1:
+        r1 = random.randint(0, len(list_of_war) - 1)
+        r2 = r1
+        while r1 == r2:
+            r2 = random.randint(0, len(list_of_war) - 1)
+
+        list_of_war[r1].get_hit(list_of_war[r2])
+        if list_of_war[r1].check_death():
+            del list_of_war[r1]
+ 
     print('++++++++++++++++++++++++++++++')
     print('\n\t\t Турнир окончен!')
     print('\tПобеду одерживает %s!' % (list_of_war.pop().get_name()))
